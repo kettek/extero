e566404dc84d5117d7f6cc502b95a8ba01ecf906
kts of kettek (yan) <kts@kettek.net>
2021-12-31 10:27:42 -0800

    Add common

---
+++ packages/common/src/api.ts
@@ -0,0 +1,32 @@
+export interface HelloMessage {
+  type: 'hello'
+  peerID: string
+}
+
+export interface JoinRoomMessage {
+  type: 'join-room'
+  room: string
+  members: string[]
+  success: boolean
+}
+export function isJoinRoomMessage(o: any): o is JoinRoomMessage {
+  return o.type === 'join-room'
+}
+
+export interface MemberJoinMessage {
+  type: 'member-join'
+  room: string
+  peerID: string
+}
+export function isMemberJoinMessage(o: any): o is MemberJoinMessage {
+  return o.type === 'member-join'
+}
+
+export interface MemberLeftMessage {
+  type: 'member-left'
+  room: string
+  peerID: string
+}
+export function isMemberLeftMessage(o: any): o is MemberLeftMessage {
+  return o.type === 'member-left'
+}
