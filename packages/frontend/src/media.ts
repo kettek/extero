b650f7d7da304c0bf4bc82c0c7e7d30024d8ed6e
kts of kettek <kts@kettek.net>
2022-01-07 07:52:39 -0800

    Extend interface

--- packages/frontend/src/media.ts
+++ packages/frontend/src/media.ts
@@ -4,4 +4,10 @@ export interface Media {
   mediaType: MediaType
   uuid: string
   stream: MediaStream
+  device?: string
+  width?: number
+  height?: number
+  framerate?: number
+  facing?: 'user'|'environment'
+  captureAudio?: boolean
 }
