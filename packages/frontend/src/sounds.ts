ccf27393a81734f8bc9cfb63a7b222ae51c9d0fc
kts of kettek <kts@kettek.net>
2022-01-13 08:06:08 -0800

    Respect sound.play promise

--- packages/frontend/src/sounds.ts
+++ packages/frontend/src/sounds.ts
@@ -2,5 +2,7 @@ export function playSound(src: string) {
   console.log('playSound', src)
   const sound = new Audio(`sounds/${src}.flac`)
   sound.volume = 0.05
-  sound.play()
+  sound.addEventListener('canplaythrough', async () => {
+    await sound.play()
+  })
 }
