dbf958a8a0f4e57fed2674ccd8104fa56fa84ea0
kts of kettek <kts@kettek.net>
2022-01-14 12:18:22 -0800

    Add basic settings component

---
+++ packages/frontend/src/components/Settings.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+  import { settingsStore } from '../stores/settings'
+</script>
+
+<main>
+  <section class='group'>
+    <label>
+      <span>Sound Effects</span>
+      <input type='checkbox' bind:checked={$settingsStore.soundEffectsEnabled}>
+    </label>
+    <label>
+      <span>Sound Volume</span>
+      <input type='number' min=0 max=200 bind:value={$settingsStore.soundEffectsVolume}>
+    </label>
+  </section>
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+    overflow: auto;
+    background: #000;
+    padding: 1em;
+  }
+  section.group {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr);
+  }
+</style>
