153186ce6f4af86b2d437cf9f97f814270312c5c
kts of kettek <kts@kettek.net>
2022-02-05 16:29:50 -0800

    Add Button component

---
+++ packages/frontend/src/components/Button.svelte
@@ -0,0 +1,37 @@
+<script type='ts'>
+
+  export let icon: string = ''
+  export let large: boolean = false
+  export let alt: string = ''
+</script>
+
+<button title={alt} on:click class:large class:iconOnly={icon}>
+  {#if icon}
+    <img src='icons/{icon}.png' alt='{icon}'>
+  {/if}
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    min-width: 1em;
+    min-height: 1em;
+    margin: 0;
+  }
+  button.iconOnly {
+    padding: 0.25em;
+    background: none;
+  }
+  button.large img {
+    height: 5em;
+  }
+  img {
+    object-fit: contain;
+    max-width: 100%;
+    max-height: 100%;
+    height: 2em;
+  }
+</style>
